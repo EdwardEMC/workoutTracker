38490bae4d7d758045f3ac41ae949614e7f15092
EMC <edwardcoad.ec@gmail.com>
2020-03-07 09:23:01 +1030

    workout model schema added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const Transaction = require("../models/workout.js");
+const Workout = require("../models/workout.js");
 
 // Below is an example of the api routes, check what data is being passed before continuing
-// router.post("/api/transaction", ({ body }, res) => {
-//     Transaction.create(body)
-//       .then(dbTransaction => {
-//         res.json(dbTransaction);
+// router.post("/api/workout", ({ body }, res) => {
+//     Workout.create(body)
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
 //       })
 //       .catch(err => {
 //         res.status(400).json(err);
