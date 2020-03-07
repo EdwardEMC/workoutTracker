0f71b3b05d3c470711feee073099c681662bfb13
EMC <edwardcoad.ec@gmail.com>
2020-03-07 14:28:02 +1030

    api routes complete, except range

--- models/workout.js
+++ models/workout.js
@@ -3,42 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter the type of exercise"    
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter the name of the exercise"  
-        },
-        duration: {
-            type: Number,
-            trim: true
-        },
-        distance: { //only used for cardio so far
-            type: Number,
-            trim: true
-        },
-        weight: {
-            type: Number,
-            trim: true
-        },
-        reps: {
-            type: Number,
-            trim: true
-        },
-        sets: {
-            type: Number,
-            trim: true
-        }
-    }],
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: []
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,25 +2,48 @@ const Workout = require("../models/workout.js");
 
 // Below is an example of the api routes, check what data is being passed before continuing
 module.exports = function(app) {
-  app.get("/api/workout", (req, res) => {
-      Workout.find({})
-          .sort({ date: -1 })
-          .then(dbWorkout => {
-              res.json(dbWorkout);
-          })
-          .catch(err => {
-              res.status(400).json(err);
-          });
-  });
-
-  //workout is either cardio or resistance with different exercises add to them!!!
-  app.post("/api/workout/:id", ({ body }, res) => {
-      Workout.create(body)
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
         .then(dbWorkout => {
+          console.log(dbWorkout);
           res.json(dbWorkout);
         })
         .catch(err => {
           res.status(400).json(err);
         });
+  });
+
+  //workout is either cardio or resistance with different exercises add to them!!!
+  app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  // adding the exercise to the workout
+  app.put("/api/workouts/:id", (req, res) => {
+    const body = req.body;
+    Workout.update({_id: req.params.id}, {$push: {exercises: body}}, (err, data) => {
+      if(err) {
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
+  });
+
+  // find the range of workouts
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({}) //add range in the parameters
+      .then(dbWorkout => {
+          res.json(dbWorkout);
+      })
+      .catch(err => {
+          res.status(400).json(err);
+      });
   });
 }
