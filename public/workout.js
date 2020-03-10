5f6666eade068507af2443f3d2813c6ab2d692dd
EMC <edwardcoad.ec@gmail.com>
2020-03-10 18:01:02 +1030

    fixed total workout duration

--- public/workout.js
+++ public/workout.js
@@ -6,13 +6,18 @@ async function initWorkout() {
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
+    let totalDuration = 0;
+    lastWorkout.exercises.forEach(exercise => {
+      totalDuration += exercise.duration;
+    });
+
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.totalDuration,
+      totalDuration: totalDuration,
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
-
+    console.log(workoutSummary);
     renderWorkoutSummary(workoutSummary);
   } else {
     renderNoWorkoutText()
